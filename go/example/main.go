8be51c8ffdaa7b914a0ba6975a6f918826b978b7
14nry <13051+14nry@users.noreply.github.com>
2025-03-13 19:32:44 +0900

    Add main.go

--- go/example/main.go
+++ go/example/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-var version string  // ビルド時にはldflagsフラグ経由でバージョンを埋め込むための変数
+var version string  // ビルド時にldflagsフラグ経由でバージョンを埋め込むための変数
 
 func main() {
 	fmt.Printf("Example %s\n", version)
